a62b03bba0d03f9caf066d1d8aa6dd809db64ce4
chubbyhippo <mkmsacc@outlook.com>
2026-05-14 08:33:23 +0700

    Add uppercase Function bean to FunctionConfig

--- src/main/java/io/github/chubbyhippo/demo/FunctionConfig.java
+++ src/main/java/io/github/chubbyhippo/demo/FunctionConfig.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Configuration
@@ -17,5 +18,10 @@ public class FunctionConfig {
             log.info("hello, function!");
             return "hello, function!";
         };
+    }
+
+    @Bean
+    public Function<String, String> uppercase() {
+        return value -> value == null ? null : value.toUpperCase();
     }
 }
