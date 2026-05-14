82f32b0f07ed58aaec8699c3d96a79a833c501ae
chubbyhippo <mkmsacc@outlook.com>
2026-05-14 08:29:11 +0700

    Log message when helloWorld Supplier bean is invoked.

--- src/main/java/io/github/chubbyhippo/demo/FunctionConfig.java
+++ src/main/java/io/github/chubbyhippo/demo/FunctionConfig.java
@@ -1,5 +1,7 @@
 package io.github.chubbyhippo.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,9 +9,13 @@ import java.util.function.Supplier;
 
 @Configuration
 public class FunctionConfig {
+    private static final Logger log = LoggerFactory.getLogger(FunctionConfig.class);
 
     @Bean
     public Supplier<String> helloWorld() {
-        return () -> "Hello, World!";
+        return () -> {
+            log.info("Hello, World!");
+            return "Hello, World!";
+        };
     }
 }
